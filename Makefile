8856813b373f231df9561b9d47d28bc556793d9d
Nacho Raik <ignacioraiksg@gmail.com>
2020-04-27 19:18:06 -0300

    Adding minimal example srv

---
+++ Makefile
@@ -0,0 +1,14 @@
+build:
+	docker build -t tu2bo-app-server .
+
+run:
+	docker run -it -p 3000:3000 tu2bo-app-server 
+
+plain-build:
+	cd src; npm install;
+
+plain-run:
+	cd src; npm start;
+
+ping:
+	curl -vvv "localhost:3000/ping"
---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const HOST = "localhost"
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+	res.status(200)
+	res.send('Hola! Le estas pegando a '+ HOST + ':' + PORT + ', donde vive el App Server!')
+});
+
+app.get('/ping', (req, res) => {
+	res.status(200)
+	res.send('Estoy vivo')
+});
+
+
+app.listen(PORT, () => console.log(`App Server levantado en puerto ${PORT}!`))
+
+process.on('SIGINT', function() {
+    process.exit();
+});
