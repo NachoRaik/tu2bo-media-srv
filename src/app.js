3b71ad1f4076a28c137a9180d0293270223553b5
Cecilia Hortas <37588761+chortas@users.noreply.github.com>
2020-06-04 15:49:13 -0300

    Api definition (#2)

--- src/app.js
+++ src/app.js
@@ -1,26 +1,28 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const app = express()
 
 const HOST = "localhost"
 const PORT = 3000;
 
-
 app.get('/', (req, res) => {
 	res.status(200)
-	res.send('Hola! Le estas pegando a '+ HOST + ':' + PORT + ', donde vive el App Server!')
+	res.send('Hi! You are hitting '+ HOST + ':' + PORT + ', where lives the Media Server!')
 });
 
 app.get('/ping', (req, res) => {
 	res.status(200)
-	res.send('Estoy vivo')
+	res.send('Im alive')
 });
 
-
-app.listen(PORT, () => console.log(`App Server levantado en puerto ${PORT}!`))
+app.listen(PORT, () => console.log(`Media server up in port ${PORT}!`))
 
 process.on('SIGINT', function() {
     process.exit();
 });
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
